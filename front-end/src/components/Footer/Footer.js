3594d1326dc918ce3ba10252f345f17984480ed8
Dale <dale@gacusan.com>
2021-01-05 13:47:43 +0800

    Filled in details for Footer

--- front-end/src/components/Footer/Footer.js
+++ front-end/src/components/Footer/Footer.js
@@ -10,8 +10,26 @@ import styles from './footer.module.css';
 
 export default function Footer() {
 	return (
-		<Grid item xs={12} className={styles.footer}>
-			<p>Footer</p>
+		<Grid container className={styles.footer}>
+			<Grid item xs={6}>
+				<p>Bayanihan</p>
+				<p>
+					A platform that connects farmers directly to end consumers <br />
+					via a blockchain-enabled crowdfunding platform.
+				</p>
+				<p>© 2020 Team Agriblockchain. All rights reserved.</p>
+			</Grid>
+			<Grid item xs={6}>
+				<p>
+					<a href='https://youtu.be/y8MjU8664Lo' target='_blank' rel='noreferrer'>
+						<img src='/images/youtube.svg' alt='youtube' className={styles.footerIcon} />
+					</a>
+				</p>
+				<p>Report a bug.</p>
+				<p>
+					<a href='https://github.com/dalegacusan/agriblockchain'>https://github.com/dalegacusan/agriblockchain</a>
+				</p>
+			</Grid>
 		</Grid>
 	);
 }
